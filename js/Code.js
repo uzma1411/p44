353839cb4f8c7fc4c50e0c13b7998d25264819c8
uzma1411 <47413610+uzma1411@users.noreply.github.com>
2021-09-21 00:09:12 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,17 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50);
+    text("C I T N O F N U",100,300);
+    text("S A E T A B D A",700,150)
+
+    fill("lightblue");
+    text("Hint: Always changing, not constant !!", 100,70);
+    text("Hint: Used to do a specific task",100,320);
+    text("Hint: You need this to play multiplayer",700,170);
+}
